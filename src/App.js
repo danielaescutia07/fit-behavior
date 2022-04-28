710ed355f007d1275d1715268e25bfdb76715835
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-28 11:42:11 -0500

    created components/homepage folders and and imported HomePage.js into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+//*Custom Components
+import HomePage from './components/HomePage/HomePage';
+
 function App() {
   return (
     <div className="App">
       <h1>Fit Behavior</h1>
       <h2>An app that will allow users to keep track of their habits to become or stay fit.</h2>
       <p>Coming Soon!</p>
+      <HomePage />
     </div>
   );
 }
