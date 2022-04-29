0b372a177d4d0754c5f8c3f01bd79cd2aeb83666
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-29 13:28:36 -0500

    implemented animation to heading and adding box shadow to image

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import './HomePage.css'
 
-// const ACCESS_KEY = 'VBhpjRajlNtDYginto40moaV5RhhtJ7wZ7HndOTJZww';
+const ACCESS_KEY = 'VBhpjRajlNtDYginto40moaV5RhhtJ7wZ7HndOTJZww';
 
 function HomePage() {
   const [ image, setImage ] = useState({});
 
-//   useEffect(() => {
-//     axios.get(`https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&query=health&orientation=landscape`)
-//       .then(res => {
-//         setImage(res.data.urls.regular)
-//       })
-//       .catch(err => {
-//         console.error(err)
-//       })
-//   }, []);
+  useEffect(() => {
+    axios.get(`https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&query=health&orientation=landscape`)
+      .then(res => {
+        setImage(res.data.urls.regular)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, []);
 
   return (
-    <div className='homePage'>
-      <img src={image} alt='HomeImage' />
-      <h3>Under Construction</h3>
+    <div className='homePage '>
+      <img className='home-image card container' src={image} alt='HomeImage' />
       <p>Motivation for a better Fit Behavior.</p>
       <button>Click here</button>
     </div>
